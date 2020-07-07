41bbb0cb85545bd93533c2ee78aa561049297eac
quocbao <bacancuoc@gmail.com>
2020-07-07 16:47:55 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import MyForm from './components/Calulator_bond';
+import YtmFrom from './components/ytmcaculator';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <MyForm />
+        </div>
+        <div>
+          <YtmFrom />
+        </div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </header>
     </div>
   );
